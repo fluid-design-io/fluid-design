8728c8b036a6159f56743dda06d353c5c7a65b45
fluid-design-io <2216991777@qq.com>
2023-10-17 23:59:48 -0500

    feat: new API

---
+++ apps/web/app/api/generate-color-palette/route.ts
@@ -0,0 +1,56 @@
+import { generateColorPalette } from "@/lib/colorCalculator";
+import { colorHelper } from "@/lib/colorHelper";
+import { BaseColorTypes, ColorMode } from "@/types/app";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+  const origin = req.headers.get("origin");
+  const { searchParams } = new URL(req.url);
+  const colorsString = searchParams.get("colors");
+  console.log({ colorsString });
+  if (!colorsString) {
+    return NextResponse.json(
+      {
+        data: null,
+        error: {
+          message: "Invalid token",
+        },
+      },
+      {
+        status: 400,
+      },
+    );
+  }
+  const baseColors = colorHelper.colorStringToBaseColors(colorsString);
+  const mode = ColorMode.HEX;
+  const [primaryPalette, secondaryPalette, accentPalette, grayPalette] = [
+    "primary",
+    "secondary",
+    "accent",
+    "gray",
+  ].map((color) =>
+    generateColorPalette({
+      color: color === "gray" ? baseColors.primary : baseColors[color],
+      type: color as BaseColorTypes,
+      colorMode: mode,
+    }),
+  );
+  return NextResponse.json(
+    {
+      data: {
+        baseColors,
+        colorPalettes: {
+          primary: primaryPalette,
+          secondary: secondaryPalette,
+          accent: accentPalette,
+          gray: grayPalette,
+        },
+        colorMode: mode,
+      },
+      error: null,
+    },
+    {
+      status: 200,
+    },
+  );
+}
