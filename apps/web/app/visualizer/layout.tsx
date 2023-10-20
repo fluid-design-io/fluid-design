aee5620d862557a4fd4d21c98a0f1ae04e52eb7e
fluid-design-io <2216991777@qq.com>
2023-10-20 00:57:35 -0500

    fix: ui updates

--- apps/web/app/visualizer/layout.tsx
+++ apps/web/app/visualizer/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }) {
   return (
     <div className="site-padding mx-auto mt-6 flex w-full max-w-[120rem] flex-1 flex-col pb-20 sm:pb-24 md:mt-8 lg:mt-10">
       <section>
-        <Card className="relative flex min-h-[80vh] flex-1 flex-col overflow-hidden">
+        <Card className="relative flex min-h-[max(80vh,960px)] flex-1 flex-col overflow-hidden">
           <VisualizerNav />
           <div className="flex h-full w-full flex-1 flex-col items-stretch justify-center">
             {children}
