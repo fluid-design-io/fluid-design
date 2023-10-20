74f090c7773e6f0034ecb74d30205cbf237a4d74
fluid-design-io <2216991777@qq.com>
2023-10-20 00:57:05 -0500

    feat: new download as png plugin

--- apps/web/components/palette/color-palette.tsx
+++ apps/web/components/palette/color-palette.tsx
@@ -6,7 +6,10 @@ const PaletteBody = dynamic(() => import("@/components/palette/palette-body"));
 
 function ColorPalette() {
   return (
-    <div className="mt-6 rounded-3xl border border-border bg-background p-4 transition-colors sm:p-6 md:mt-8 lg:mt-10 lg:p-8 xl:p-12">
+    <div
+      className="mt-6 rounded-3xl border border-border bg-background p-4 transition-colors sm:p-6 md:mt-8 lg:mt-10 lg:p-8 xl:p-12"
+      id="base-color-palette"
+    >
       <PaletteHeader />
       <PaletteBody />
     </div>
