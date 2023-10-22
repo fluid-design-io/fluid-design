f7959dd88b2c789cec9bb9c5df927aa1fff35a3d
fluid-design-io <2216991777@qq.com>
2023-10-21 20:05:07 -0500

    feat: added how page

---
+++ apps/web/app/(aux)/tutorial/figma/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FigmaTutorial() {
+  return (
+    <div>
+      <h1 className="text-3xl">Figma Tutorial</h1>
+    </div>
+  );
+}
+
+export default FigmaTutorial;
