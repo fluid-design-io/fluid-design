021cf914b6ddb618c524070b73aef71aa5a228da
Oliver Pan <2216991777@qq.com>
2023-10-24 00:26:38 -0500

    fix: ui improvements

--- apps/web/components/toolbar/shareable-link.tsx
+++ apps/web/components/toolbar/shareable-link.tsx
@@ -5,7 +5,6 @@ import {
   PopoverTrigger,
 } from "@ui/components/ui/popover";
 
-import DesktopPreviewToolbarIcon from "../ui/desktop-primary-toolbar-button";
 import primaryToolbarMenu from "../ui/primary-toolbar-menu";
 import { useEffect, useState } from "react";
 import { useColorStore } from "@/store/store";
@@ -13,6 +12,7 @@ import { colorHelper } from "@/lib/colorHelper";
 import dynamic from "next/dynamic";
 import { Skeleton } from "ui/components/ui/skeleton";
 import ToolbarMenuItem from "./toolbar-menu-item";
+import { AnimatePresence } from "framer-motion";
 
 const ShareableLinkPlugin = dynamic(
   () => import("@/components/toolbar/plugin/shareable-link.plugin"),
@@ -41,7 +41,15 @@ function ToolbarShareableLink() {
         <ToolbarMenuItem {...menuItem} />
       </PopoverTrigger>
       <PopoverContent className="w-[18rem] sm:w-[24rem]" align="end">
-        {open && <ShareableLinkPlugin colors={colors} setOpen={setOpen} />}
+        <AnimatePresence mode="wait">
+          {open && (
+            <ShareableLinkPlugin
+              key={`shareable-${open}`}
+              colors={colors}
+              setOpen={setOpen}
+            />
+          )}
+        </AnimatePresence>
       </PopoverContent>
     </Popover>
   );
