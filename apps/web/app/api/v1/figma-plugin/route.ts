17c51bfab3774208549ad2a44395b3139bfa3adf
Oliver Pan <2216991777@qq.com>
2024-06-24 12:49:34 -0500

    chore: Refactor figma-plugin route

--- apps/web/app/api/v1/figma-plugin/route.ts
+++ apps/web/app/api/v1/figma-plugin/route.ts
@@ -1,26 +1,26 @@
-import { BaseColors, RawColor } from "@/types/app";
-import jwt from "jsonwebtoken";
-import { NextResponse } from "next/server";
-import tinycolor from "tinycolor2";
+import { RawColor } from '@/types/app'
+import jwt from 'jsonwebtoken'
+import { NextResponse } from 'next/server'
+import tinycolor from 'tinycolor2'
 
 const headers = (origin: string) => ({
-  "Access-Control-Allow-Credentials": "true",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization, Accept",
-  "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-  "Access-Control-Allow-Origin": origin || "*",
-});
+  'Access-Control-Allow-Credentials': 'true',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization, Accept',
+  'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+  'Access-Control-Allow-Origin': origin || '*',
+})
 
 export async function OPTIONS(request: Request) {
-  const origin = request.headers.get("origin");
+  const origin = request.headers.get('origin')
 
   return new Response(null, {
     headers: headers(origin),
     status: 200,
-  });
+  })
 }
 export async function POST(request: Request) {
-  const { baseColors } = await request.json();
-  const authSecret = process.env.FIGMA_AUTH_SECRET;
+  const { baseColors } = await request.json()
+  const authSecret = process.env.FIGMA_AUTH_SECRET
   // generate a bearer token using jwt.io
   const token = jwt.sign(
     {
@@ -34,58 +34,76 @@ export async function POST(request: Request) {
       // this is the secret key
       // we can use the same secret key in the figma plugin to decode the token
       // and get the base colors
-      expiresIn: "30d",
+      expiresIn: '30d',
     },
-  );
+  )
 
   return NextResponse.json({
     data: {
       token,
     },
     error: null,
-  });
+  })
 }
 
 export async function GET(req: Request) {
-  const origin = req.headers.get("origin");
-  const { searchParams } = new URL(req.url);
-  const token = searchParams.get("token");
-  const authSecret = process.env.FIGMA_AUTH_SECRET;
+  const origin = req.headers.get('origin')
+  const { searchParams } = new URL(req.url)
+  const token = searchParams.get('token')
+  const authSecret = process.env.FIGMA_AUTH_SECRET
   // decode the token using the same secret key
   // we should get the base colors
-  let baseColors: Partial<BaseColors>;
+  /*
+  https://colors.fluid-design.io/?primary=fb46d6&secondary=e82c21&accent=9b2c97&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcmltYXJ5Ijp7ImEiOjEsImgiOjMxMi4yNjUxOTMzNzAxNjU4LCJsIjowLjYyOTQxMTc2NDcwNTg4MjMsInMiOjAuOTU3NjcxOTU3NjcxOTU3N30sInNlY29uZGFyeSI6eyJhIjoxLCJoIjozLjMxNjU4MjkxNDU3Mjg2MywibCI6MC41MTk2MDc4NDMxMzcyNTQ5LCJzIjowLjgxMjI0NDg5Nzk1OTE4MzV9LCJhY2NlbnQiOnsiYSI6MSwiaCI6MzAyLjE2MjE2MjE2MjE2MjEzLCJsIjowLjM5MDE5NjA3ODQzMTM3MjUsInMiOjAuNTU3Nzg4OTQ0NzIzNjE4MX0sImlhdCI6MTcxOTI1MTEzMiwiZXhwIjoxNzIxODQzMTMyfQ.H2gg_nCxbEC4NbZ1NLci8PRU0hPMe1_iGnxkCJZ42JQ
+  */
   try {
-    baseColors = jwt.verify(token, authSecret);
+    jwt.verify(token, authSecret)
   } catch (error) {
-    console.log(`====> Error:`, error);
+    console.log(`====> Error:`, error)
     return NextResponse.json(
       {
         data: null,
         error: {
-          message: "Invalid token",
+          message: 'Invalid token',
         },
       },
       {
         headers: headers(origin),
         status: 400,
       },
-    );
+    )
   }
-  const { accent, primary, secondary } = baseColors as BaseColors;
-  const v = (color: RawColor | string) => tinycolor(color).isValid();
+  const primary = searchParams.get('primary')
+  const secondary = searchParams.get('secondary')
+  const accent = searchParams.get('accent')
+  if (!primary || !secondary || !accent) {
+    return NextResponse.json(
+      {
+        data: null,
+        error: {
+          message: 'Primary, secondary or accent color is missing',
+        },
+      },
+      {
+        headers: headers(origin),
+        status: 400,
+      },
+    )
+  }
+  const v = (color: RawColor | string) => tinycolor(color).isValid()
   if (!v(primary) || !v(secondary) || !v(accent)) {
     return NextResponse.json(
       {
         data: null,
         error: {
-          message: "Invalid primary, secondary or accent color",
+          message: 'Invalid primary, secondary or accent color',
         },
       },
       {
         headers: headers(origin),
         status: 400,
       },
-    );
+    )
   }
   return NextResponse.json(
     {
@@ -99,5 +117,5 @@ export async function GET(req: Request) {
     {
       headers: headers(origin),
     },
-  );
+  )
 }
