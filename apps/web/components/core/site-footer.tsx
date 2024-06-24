d7f0f57407eb091f7b27c02b94f5a51a998e4b22
Oliver Pan <2216991777@qq.com>
2024-06-24 00:26:57 -0500

    chore: refactor footer

--- apps/web/components/core/site-footer.tsx
+++ apps/web/components/core/site-footer.tsx
@@ -1,47 +1,29 @@
-import { Github, Youtube } from "lucide-react";
-import app from "../../../../package.json";
-import { cn } from "ui/lib/utils";
+import { cn } from 'ui/lib/utils'
 
-const navigation = [
-  {
-    name: "GitHub",
-    href: "https://github.com/fluid-design-io/fluid-design",
-    icon: Github,
-  },
-  {
-    name: "YouTube",
-    href: "https://youtube.com/@fluiddesign",
-    icon: Youtube,
-  },
-];
+import Link from 'next/link'
+import app from '../../../../package.json'
 
 function SiteFooter() {
   return (
     <footer
       className={cn(
-        "border-t border-border/30 bg-background-accent",
-        "pb-[calc(8rem+env(safe-area-inset-bottom))] lg:pb-0",
+        'transition-bg border-t border-border/30 bg-background-accent',
+        'pb-[calc(8rem+env(safe-area-inset-bottom))] lg:pb-0'
       )}
     >
       <div className="mx-auto max-w-[120rem] px-6 py-4 md:flex md:items-center md:justify-between lg:px-8">
         <div className="flex justify-center space-x-6 md:order-2">
-          {navigation.map((item) => (
-            <a
-              key={item.name}
-              href={item.href}
-              className="text-foreground/50 hover:text-primary/80"
-            >
-              <span className="sr-only">{item.name}</span>
-              <item.icon className="h-4 w-4" aria-hidden="true" />
-            </a>
-          ))}
+          <Link href="/how" className="text-xs text-foreground/50 underline">
+            How it works
+          </Link>
+
           <a
+            aria-label="Release Notes"
+            className="text-[0.7rem] text-foreground/50 hover:underline"
             href="https://github.com/fluid-design-io/fluid-design/releases"
-            target="_blank"
             referrerPolicy="no-referrer"
             rel="noopener noreferrer"
-            className="text-[0.7rem] text-foreground/50 hover:underline"
-            aria-label="Release Notes"
+            target="_blank"
             title="Release Notes"
           >
             V {app.version}
@@ -49,25 +31,25 @@ function SiteFooter() {
         </div>
         <div className="mt-8 md:order-1 md:mt-0">
           <p className="text-center text-xs leading-5 text-gray-500">
-            Built by{" "}
+            Built by{' '}
             <a
-              href="https://oliverpan.vercel.app"
-              target="_blank"
               className="underline"
+              href="https://oliverpan.vercel.app"
               referrerPolicy="no-referrer"
               rel="noopener noreferrer"
+              target="_blank"
               title="Visit Oliver's portfolio"
             >
               Oliver
             </a>
-            {". "}
-            The source code is on{" "}
+            {'. '}
+            The source code is on{' '}
             <a
-              href="https://github.com/fluid-design-io/fluid-design"
-              target="_blank"
               className="underline"
+              href="https://github.com/fluid-design-io/fluid-design"
               referrerPolicy="no-referrer"
               rel="noopener noreferrer"
+              target="_blank"
             >
               GitHub
             </a>
@@ -76,7 +58,7 @@ function SiteFooter() {
         </div>
       </div>
     </footer>
-  );
+  )
 }
 
-export default SiteFooter;
+export default SiteFooter
