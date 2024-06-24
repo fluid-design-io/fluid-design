1fc097748a79736e1f1bc0a71e71b543fe2dbec1
Oliver Pan <2216991777@qq.com>
2024-06-24 00:35:52 -0500

    refactor: Update color generation logic and fix linting issues

--- apps/web/types/app.ts
+++ apps/web/types/app.ts
@@ -1,71 +1,82 @@
 export enum ColorMode {
-  HEX = "hex",
-  RGB = "rgb",
-  HSL = "hsl",
+  HEX = 'hex',
+  RGB = 'rgb',
+  HSL = 'hsl',
 }
 
 export type RawColor = {
-  h: number;
-  s: number;
-  l: number;
-  a: number;
-};
-
-export type RGB = {
-  r: number;
-  g: number;
-  b: number;
-};
-
-export type BaseColorTypes = "primary" | "secondary" | "accent" | "gray";
-
-export type BaseColors = Record<BaseColorTypes, RawColor>;
-
-export type ColorReadability = {
-  readability: number;
-  isReadable: boolean;
-};
-
-export enum CVDType {
-  DeficiencyProt = "protanomaly and protanopia",
-  DeficiencyDeuter = "deuteranomaly and deuteranopia",
-  DeficiencyTrit = "tritanomaly and tritanopia",
+  h: number
+  s: number
+  l: number
+  a: number
 }
 
-export type ColorValue =
-  | {
-      step: number;
-      color: string;
-      raw: RawColor;
-      /**
-       * Readability is calculated using the WCAG 2.0 formula
-       * Compares the contrast ratio between the foreground and background colors
-       */
-      readability?: {
-        foreground: ColorReadability;
-        background: ColorReadability;
-      };
-    }
-  | undefined;
+export type RGB = {
+  r: number
+  g: number
+  b: number
+}
 
-export type ColorPalettes = Record<BaseColorTypes, ColorValue[]>;
+export type ColorNames = string[]
+
+export type BaseColorTypes = 'primary' | 'secondary' | 'accent' | 'gray'
+
+export type BaseColors = Record<BaseColorTypes, RawColor>
+
+export type ColorOptions = Omit<Record<BaseColorTypes, string>, 'gray'>
+
+export type GeneratedColors = {
+  baseColors: {
+    primary: RawColor
+    secondary: RawColor
+    accent: RawColor
+  }
+  colorPalettes: ColorPalettes
+}
+
+export type ColorReadability = {
+  readability: number
+  isReadable: boolean
+}
+
+export enum CVDType {
+  DeficiencyProt = 'protanomaly and protanopia',
+  DeficiencyDeuter = 'deuteranomaly and deuteranopia',
+  DeficiencyTrit = 'tritanomaly and tritanopia',
+}
+
+export type ColorValue = {
+  step: number
+  color: string
+  raw: RawColor
+  /**
+   * Readability is calculated using the WCAG 2.0 formula
+   * Compares the contrast ratio between the foreground and background colors
+   */
+  readability?: {
+    foreground: ColorReadability
+    background: ColorReadability
+  }
+}
+
+export type ColorPalettes = Record<BaseColorTypes, ColorValue[]>
 
 export type AppCSSVariables =
-  | "--background"
-  | "--foreground"
-  | "--card"
-  | "--card-foreground"
-  | "--popover"
-  | "--popover-foreground"
-  | "--primary"
-  | "--primary-foreground"
-  | "--secondary"
-  | "--secondary-foreground"
-  | "--muted"
-  | "--muted-foreground"
-  | "--accent"
-  | "--accent-foreground"
-  | "--border"
-  | "--input"
-  | "--ring"
-  | "--background-accent";
+  | '--background'
+  | '--foreground'
+  | '--card'
+  | '--card-foreground'
+  | '--popover'
+  | '--popover-foreground'
+  | '--primary'
+  | '--primary-foreground'
+  | '--secondary'
+  | '--secondary-foreground'
+  | '--muted'
+  | '--muted-foreground'
+  | '--accent'
+  | '--accent-foreground'
+  | '--border'
+  | '--input'
+  | '--ring'
+  | '--background-accent'
