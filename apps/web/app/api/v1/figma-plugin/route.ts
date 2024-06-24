23f35ff5f385d0280f0aebaddda3df678215b5bc
Oliver Pan <2216991777@qq.com>
2024-06-24 12:58:41 -0500

    fix: rolled back

--- apps/web/app/api/v1/figma-plugin/route.ts
+++ apps/web/app/api/v1/figma-plugin/route.ts
@@ -1,4 +1,4 @@
-import { RawColor } from '@/types/app'
+import { BaseColors, RawColor } from '@/types/app'
 import jwt from 'jsonwebtoken'
 import { NextResponse } from 'next/server'
 import tinycolor from 'tinycolor2'
@@ -53,11 +53,9 @@ export async function GET(req: Request) {
   const authSecret = process.env.FIGMA_AUTH_SECRET
   // decode the token using the same secret key
   // we should get the base colors
-  /*
-  https://colors.fluid-design.io/?primary=fb46d6&secondary=e82c21&accent=9b2c97&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcmltYXJ5Ijp7ImEiOjEsImgiOjMxMi4yNjUxOTMzNzAxNjU4LCJsIjowLjYyOTQxMTc2NDcwNTg4MjMsInMiOjAuOTU3NjcxOTU3NjcxOTU3N30sInNlY29uZGFyeSI6eyJhIjoxLCJoIjozLjMxNjU4MjkxNDU3Mjg2MywibCI6MC41MTk2MDc4NDMxMzcyNTQ5LCJzIjowLjgxMjI0NDg5Nzk1OTE4MzV9LCJhY2NlbnQiOnsiYSI6MSwiaCI6MzAyLjE2MjE2MjE2MjE2MjEzLCJsIjowLjM5MDE5NjA3ODQzMTM3MjUsInMiOjAuNTU3Nzg4OTQ0NzIzNjE4MX0sImlhdCI6MTcxOTI1MTEzMiwiZXhwIjoxNzIxODQzMTMyfQ.H2gg_nCxbEC4NbZ1NLci8PRU0hPMe1_iGnxkCJZ42JQ
-  */
+  let baseColors: Partial<BaseColors>
   try {
-    jwt.verify(token, authSecret)
+    baseColors = jwt.verify(token, authSecret)
   } catch (error) {
     console.log(`====> Error:`, error)
     return NextResponse.json(
@@ -73,23 +71,7 @@ export async function GET(req: Request) {
       },
     )
   }
-  const primary = searchParams.get('primary')
-  const secondary = searchParams.get('secondary')
-  const accent = searchParams.get('accent')
-  if (!primary || !secondary || !accent) {
-    return NextResponse.json(
-      {
-        data: null,
-        error: {
-          message: 'Primary, secondary or accent color is missing',
-        },
-      },
-      {
-        headers: headers(origin),
-        status: 400,
-      },
-    )
-  }
+  const { accent, primary, secondary } = baseColors as BaseColors
   const v = (color: RawColor | string) => tinycolor(color).isValid()
   if (!v(primary) || !v(secondary) || !v(accent)) {
     return NextResponse.json(
