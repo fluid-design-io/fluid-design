d85a1e3bdba5b7b458d03252239c6bcfec602bdf
Oliver Pan <2216991777@qq.com>
2024-06-24 00:25:25 -0500

    chore: Refactor color picker fab button component

--- apps/web/components/core/color-picker-fab-button.tsx
+++ apps/web/components/core/color-picker-fab-button.tsx
@@ -1,51 +1,50 @@
-"use client";
+'use client'
 
-import { useColorStore } from "@/store/store";
-import { BaseColorTypes, ColorMode } from "@/types/app";
-import { cn } from "@ui/lib/utils";
-import { motion, AnimatePresence } from "framer-motion";
-import ColorPickerModal from "../palette/color-picker-modal";
-import { useState } from "react";
-import { colorHelper } from "@/lib/colorHelper";
+import { useColorStore } from '@/context/color-store-provider'
+import { colorHelper } from '@/lib/colorHelper'
+import { ColorMode, ColorOptions } from '@/types/app'
+import { cn } from '@ui/lib/utils'
+import { AnimatePresence, motion } from 'framer-motion'
+import { useState } from 'react'
 
-const ColorPickerFabButton = ({ type }: { type: BaseColorTypes }) => {
-  const { baseColors, updateBaseColor } = useColorStore();
-  const [isOpen, setIsOpen] = useState(false);
-  const colorString = colorHelper.toColorMode(baseColors[type], ColorMode.HEX);
+import ColorPickerModal from '../palette/color-picker-modal'
+
+const ColorPickerFabButton = ({ type }: { type: keyof ColorOptions }) => {
+  const [isOpen, setIsOpen] = useState(false)
+  const {
+    setColor,
+    colors: { baseColors },
+  } = useColorStore((s) => s)
+  const colorString = colorHelper.toColorMode(baseColors[type], ColorMode.HEX)
   const shadowColor = {
-    primary: "hover:shadow-primary/30",
-    secondary: "hover:shadow-secondary/30",
-    accent: "hover:shadow-accent/30",
-  };
+    accent: 'hover:shadow-accent/30',
+    primary: 'hover:shadow-primary/30',
+    secondary: 'hover:shadow-secondary/30',
+  }
   return (
-    <AnimatePresence mode="sync" initial={false}>
+    <AnimatePresence initial={false} mode="sync">
       <motion.button
-        className={cn(
-          "relative h-12 w-12 shadow-lg transition-shadow hover:shadow-xl",
-          shadowColor[type],
-        )}
-        onClick={() => setIsOpen(true)}
+        animate={{ borderRadius: 48, scale: 1 }}
+        className={cn('relative h-12 w-12 shadow-lg transition-shadow hover:shadow-xl', shadowColor[type])}
+        exit={{ borderRadius: 16, scale: 0.88 }}
+        initial={{ borderRadius: 16, scale: 0.88 }}
         key={`color-picker-${type}`}
-        initial={{ scale: 0.88, borderRadius: 16 }}
-        animate={{ scale: 1, borderRadius: 48 }}
-        exit={{ scale: 0.88, borderRadius: 16 }}
+        layoutId={`color-picker-${type}-fab`}
+        onClick={() => setIsOpen(true)}
+        transition={{
+          bounce: 0.2,
+          type: 'spring',
+        }}
         whileHover={{ scale: 1.05 }}
         whileTap={{ scale: 0.95 }}
-        transition={{
-          type: "spring",
-          bounce: 0.2,
-        }}
-        layoutId={`color-picker-${type}-fab`}
       >
         <motion.span
-          key={`color-picker-${type}-fill`}
           className={cn(
-            "absolute inset-0 rounded-full transition-all",
-            "ring-2 ring-inset",
-            isOpen
-              ? "ring-transparent"
-              : "ring-white delay-500 duration-1000 dark:ring-white/40",
+            'absolute inset-0 rounded-full transition-all',
+            'ring-2 ring-inset',
+            isOpen ? 'ring-transparent' : 'ring-white delay-500 duration-1000 dark:ring-white/40'
           )}
+          key={`color-picker-${type}-fill`}
           style={{ backgroundColor: colorString }}
         />
         <span className="sr-only">Click to change {type} color</span>
@@ -53,15 +52,15 @@ const ColorPickerFabButton = ({ type }: { type: BaseColorTypes }) => {
       </motion.button>
       {isOpen && (
         <ColorPickerModal
-          onClose={() => setIsOpen(false)}
           colorString={colorString}
-          onChange={updateBaseColor}
-          type={type}
+          onChange={(k, c) => setColor(k, colorHelper.toHex(c))}
+          onClose={() => setIsOpen(false)}
           prefix="fab"
+          type={type}
         />
       )}
     </AnimatePresence>
-  );
-};
+  )
+}
 
-export default ColorPickerFabButton;
+export default ColorPickerFabButton
